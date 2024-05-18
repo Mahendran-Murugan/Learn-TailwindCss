0f89e62d0ca365d27306b0a10a5c75892a24192a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-18 17:07:13 +0530

    feat: learn break points

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <section className='h-screen bg-green-200 lg:bg-red-500 flex items-center justify-center text-3xl p-12 mx-12'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam voluptatem id, temporibus blanditiis magni, atque consequatur fugit modi et saepe ducimus tenetur reiciendis quod assumenda laudantium doloremque nulla corporis quos.
+        </p>
+      </section>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx.ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
