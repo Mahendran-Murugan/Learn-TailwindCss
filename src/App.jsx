20f9aabe2f7fc55b21cf0428cfffa83220439c18
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-20 19:52:37 +0530

    feat: learn responsive design for equal size components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { OwnCardComponent } from './components/OwnCardComponent'
 import { MultiViewPort } from './components/MultiViewPort'
 import { SideBar } from './components/SideBar'
+import { LayoutDesign } from './components/LayoutDesign'
 
 export const App = () => {
   return (
     <div>
-      <SideBar />
+      {/* <SideBar /> */}
+      <LayoutDesign />
     </div>
   )
 }
