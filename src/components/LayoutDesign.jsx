f9b79ea47ac4ba8600ef79f4484709c410883447
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-20 20:00:52 +0530

    feat: learn responsive design for non equal size components

--- src/components/LayoutDesign.jsx
+++ src/components/LayoutDesign.jsx
@@ -7,11 +7,17 @@ export const LayoutDesign = () => {
         //     <div className='min-h-[100px] rounded-lg shadow bg-red-300'></div>
         //     <div className='min-h-[100px] rounded-lg shadow bg-green-300'></div>
         // </div>
-        <div className='grid sm:grid-cols-4 gap-4 m-4'>
-            <div className='min-h-[100px] rounded-lg shadow bg-red-300'></div>
-            <div className='min-h-[100px] rounded-lg shadow bg-green-300'></div>
-            <div className='min-h-[100px] rounded-lg shadow bg-blue-300'></div>
-            <div className='min-h-[100px] rounded-lg shadow bg-purple-300'></div>
+
+        // <div className='grid sm:grid-cols-4 gap-4 m-4'>
+        //     <div className='min-h-[100px] rounded-lg shadow bg-red-300'></div>
+        //     <div className='min-h-[100px] rounded-lg shadow bg-green-300'></div>
+        //     <div className='min-h-[100px] rounded-lg shadow bg-blue-300'></div>
+        //     <div className='min-h-[100px] rounded-lg shadow bg-purple-300'></div>
+        // </div>
+
+        <div className='grid sm:grid-cols-12 gap-4 m-4'>
+            <div className='min-h-[100px] rounded-lg shadow bg-red-300 sm:col-span-2'></div>
+            <div className='min-h-[100px] rounded-lg shadow bg-green-300 sm:col-span-10'></div>
         </div>
     )
 }
