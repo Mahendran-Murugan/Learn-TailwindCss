bc3c62279b62a23895ffa806776a9fd476472064
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-31 20:26:05 +0530

    feat: complete glassmorph effect

--- src/components/GlassMorphism.jsx
+++ src/components/GlassMorphism.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const GlassMorphism = () => {
     return (
-        <div>GlassMorphism</div>
+        <div className='h-screen  bg-gray-950'>
+            <div className='circle'>
+                <div className='relative h-[200px] w-[350px] bg-white/10 z-10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl'>
+
+                </div>
+            </div>
+        </div>
     )
 }
