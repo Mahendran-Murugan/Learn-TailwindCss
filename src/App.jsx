220c506bc462ec1b7894ab772670cfadfc9fbd77
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-31 20:13:47 +0530

    feat: add component for glass morph and fix typo in css

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { MultiViewPort } from "./components/MultiViewPort";
 import { SideBar } from "./components/SideBar";
 import { LayoutDesign } from "./components/LayoutDesign";
 import { Navbar } from "./components/Navbar";
+import { GlassMorphism } from "./components/GlassMorphism";
 
 export const App = () => {
   return (
     <div className="h-screen bg-red-400">
       {/* <SideBar /> */}
       {/* <LayoutDesign /> */}
-      <Navbar />
+      {/* <Navbar /> */}
+      <GlassMorphism />
     </div>
   );
 };
